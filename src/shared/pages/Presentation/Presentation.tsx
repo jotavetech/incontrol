9f3832886307b87e1f650a956a47bb53d1010ba2
jaoincode <ojoaogugel@gmail.com>
2023-04-01 13:26:01 -0300

    feat: why use incontrol section on presentation page

--- src/shared/pages/Presentation/Presentation.tsx
+++ src/shared/pages/Presentation/Presentation.tsx
@@ -6,7 +6,7 @@ export function Presentation() {
   return (
     <div>
       <UnloggedHead />
-      <section className="text-center mt-10 md:mt-32 flex flex-col items-center">
+      <section className="text-center mt-10 md:mt-32 flex flex-col items-center px-4">
         <h1 className="text-3xl md:text-5xl font-semibold">
           Welcome to <span className="text-secondary-color">inControl</span>
         </h1>
@@ -21,8 +21,8 @@ export function Presentation() {
           and cash inflows
         </p>
       </section>
-      <section className="bg-list-bg text-center mt-10 md:mt-32 flex flex-col items-center pb-20 md:pb-24">
-        <h1 className="text-3xl md:text-5xl font-semibold mt-10">
+      <section className="bg-list-bg text-center mt-10 md:mt-32 flex flex-col items-center pb-10 pt-10 md:pb-24 md:pt-24 px-4">
+        <h1 className="text-3xl md:text-5xl font-semibold ">
           Create an <span className="text-secondary-color">Account</span>
         </h1>
         <p className="font-medium md:mt-3 md:text-3xl max-w-[600px]">
@@ -42,6 +42,40 @@ export function Presentation() {
           >
             Login
           </Link>
+        </div>
+      </section>
+      <section className="text-center mt-7 md:mt-20 pb-32 flex flex-col items-center px-4">
+        <h1 className="text-3xl md:text-5xl font-semibold mt-10">
+          Why use <span className="text-secondary-color">inControl</span>?
+        </h1>
+        <p className="font-medium md:mt-3 md:text-3xl max-w-[600px]">
+          Perhaps you are wondering why to use InControl, so here are some
+          advantages:
+        </p>
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-5 mt-10">
+          <div className="bg-secondary-color h-72 flex flex-col items-center justify-center p-10 md:max-w-lg hover:scale-105 cursor-pointer transition-transform">
+            <img src="/assets/img/filter.svg" className="w-28" />
+            <p className="font-semibold text-xl">Filter by order or by month</p>
+          </div>
+          <div className="bg-secondary-color-2 h-72 flex flex-col items-center justify-center p-10 md:max-w-lg hover:scale-105 cursor-pointer transition-transform">
+            <img src="/assets/img/graph.svg" className="w-28" />
+            <p className="font-semibold text-xl">
+              See the money flowing in and out seamlessly
+            </p>
+          </div>
+          <div className="bg-secondary-color h-72 flex flex-col items-center justify-center p-10 md:max-w-lg hover:scale-105 cursor-pointer transition-transform">
+            <img src="/assets/img/pig.svg" className="w-28" />
+            <p className="font-semibold text-xl">
+              Separate money from the rest in the form of a piggy bank
+            </p>
+          </div>
+          <div className="bg-secondary-color-2 h-72 flex flex-col items-center justify-center p-10 md:max-w-lg hover:scale-105 cursor-pointer transition-transform">
+            <img src="/assets/img/money.svg" className="w-28" />
+            <p className="font-semibold text-xl">
+              See how much of your monthly money is left without boring
+              calculations
+            </p>
+          </div>
         </div>
       </section>
     </div>
