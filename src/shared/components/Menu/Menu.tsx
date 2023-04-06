ebd744ea5378c33bfe17f368cd338cd5627d870c
jaoincode <ojoaogugel@gmail.com>
2023-04-06 10:42:18 -0300

    feat: navigate func

--- src/shared/components/Menu/Menu.tsx
+++ src/shared/components/Menu/Menu.tsx
@@ -4,8 +4,17 @@ import { BsPiggyBank } from "react-icons/bs";
 import { GiPayMoney, GiReceiveMoney } from "react-icons/gi";
 import { AiOutlineHome, AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
+import { useNavigate } from "react-router-dom";
+
 export function Menu() {
   const [opened, setOpened] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleNavigate = (link: string) => {
+    setOpened(false);
+    navigate(link);
+  };
 
   return (
     <>
@@ -18,7 +27,7 @@ export function Menu() {
         <AiOutlineMenu />
       </button>
       <aside
-        className={`animeOpenMenu pt-20 md:pt-28 p-2 bg-list-bg h-screen w-48 md:w-60 ${
+        className={`animeOpenMenu pt-16 md:pt-28 p-2 bg-list-bg h-screen w-48 md:w-60 ${
           opened ? "block" : "hidden"
         } md:block z-40`}
       >
@@ -36,25 +45,37 @@ export function Menu() {
         <nav className="p-5">
           <ul className="flex flex-col gap-4 items-center">
             <li>
-              <button className="py-2 px-5 bg-item-bg rounded-lg w-36 md:w-44 flex items-center gap-2 justify-between text-sm md:text-base hover:bg-secondary-color">
+              <button
+                onClick={() => handleNavigate("/home")}
+                className="py-2 px-5 bg-item-bg rounded-lg w-36 md:w-44 flex items-center gap-2 justify-between text-sm md:text-base hover:bg-secondary-color"
+              >
                 Home
                 <AiOutlineHome />
               </button>
             </li>
             <li>
-              <button className="py-2 px-5 bg-item-bg rounded-lg w-36 md:w-44 flex items-center gap-2 justify-between text-sm md:text-base hover:bg-secondary-color">
+              <button
+                onClick={() => handleNavigate("/entries")}
+                className="py-2 px-5 bg-item-bg rounded-lg w-36 md:w-44 flex items-center gap-2 justify-between text-sm md:text-base hover:bg-secondary-color"
+              >
                 Entries
                 <GiReceiveMoney />
               </button>
             </li>
             <li>
-              <button className="py-2 px-5 bg-item-bg rounded-lg w-36 md:w-44 flex items-center gap-2 justify-between text-sm md:text-base hover:bg-secondary-color">
+              <button
+                onClick={() => handleNavigate("/spents")}
+                className="py-2 px-5 bg-item-bg rounded-lg w-36 md:w-44 flex items-center gap-2 justify-between text-sm md:text-base hover:bg-secondary-color"
+              >
                 Spents
                 <GiPayMoney />
               </button>
             </li>
             <li>
-              <button className="py-2 px-5 bg-item-bg rounded-lg w-36 md:w-44 flex items-center gap-2 justify-between text-sm md:text-base hover:bg-secondary-color">
+              <button
+                onClick={() => handleNavigate("/piggybank")}
+                className="py-2 px-5 bg-item-bg rounded-lg w-36 md:w-44 flex items-center gap-2 justify-between text-sm md:text-base hover:bg-secondary-color"
+              >
                 Piggy Bank
                 <BsPiggyBank />
               </button>
