c693b2b2d4a16ca3ca2a2c371425c961f64c9c15
jaoincode <ojoaogugel@gmail.com>
2023-04-08 15:10:47 -0300

    feat: changing footer bg color

--- src/shared/components/Footer/Footer.tsx
+++ src/shared/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className="mt-4 fixed bottom-0 min-w-full bg-list-bg text-center py-2 text-sm md:text-base z-50">
+    <footer className="mt-4 fixed bottom-0 min-w-full bg-item-bg text-center py-2 text-sm md:text-base z-50">
       <span className="text-white">InControl | </span>
       <a
         href="https://github.com/jaoincode
