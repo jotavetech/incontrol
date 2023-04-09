b21ffb26306a98aed86383781a6f8afc77562b61
jaoincode <ojoaogugel@gmail.com>
2023-04-09 12:30:15 -0300

    feat: shorcut icon

--- src/shared/components/MenuList/MenuList.tsx
+++ src/shared/components/MenuList/MenuList.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { MdShortcut } from "react-icons/md";
 
 import { MenuListType } from "./MenuList.types";
 
@@ -7,9 +8,11 @@ export function MenuList({ type, children }: MenuListType) {
     <div className="w-full md:w-[50%] h-[230px] md:h-[300px] max-h-[300px] bg-list-bg rounded-xl p-2 md:p-5 relative">
       <Link
         to={`/${type}`}
-        className="absolute -top-10 font-semibold text-xl left-2"
+        className="absolute -top-10 font-semibold text-xl left-2 flex items-center gap-1"
       >
         {type === "entries" ? "Entries" : "Spents"}
+        <MdShortcut className="fill-secondary-color" />
+        <span className="text-[10px] text-gray-300">(click here)</span>
       </Link>
       {children}
     </div>
