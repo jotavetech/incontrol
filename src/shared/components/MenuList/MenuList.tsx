e9f90d7a84a81419a9905a835755b4877567df4a
jaoincode <ojoaogugel@gmail.com>
2023-04-08 22:09:01 -0300

    feat: link to other pages

--- src/shared/components/MenuList/MenuList.tsx
+++ src/shared/components/MenuList/MenuList.tsx
@@ -1,11 +1,16 @@
+import { Link } from "react-router-dom";
+
 import { MenuListType } from "./MenuList.types";
 
 export function MenuList({ type, children }: MenuListType) {
   return (
     <div className="w-full md:w-[50%] h-[230px] md:h-[300px] max-h-[300px] bg-list-bg rounded-xl p-2 md:p-5 relative">
-      <h1 className="absolute -top-10 font-semibold text-xl left-2">
+      <Link
+        to={`/${type}`}
+        className="absolute -top-10 font-semibold text-xl left-2"
+      >
         {type === "entries" ? "Entries" : "Spents"}
-      </h1>
+      </Link>
       {children}
     </div>
   );
