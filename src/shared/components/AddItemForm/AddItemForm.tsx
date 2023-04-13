dc49def25adda87d607b2a83d5330dcf57f7734e
jaoincode <ojoaogugel@gmail.com>
2023-04-13 08:56:11 -0300

    feat: create item on db

--- src/shared/components/AddItemForm/AddItemForm.tsx
+++ src/shared/components/AddItemForm/AddItemForm.tsx
@@ -1,19 +1,37 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
+
 import { Input } from "../";
-import { AddItemFormType } from "./AddItemForm.types";
+
+import { AddItemFormType, ItemType } from "./AddItemForm.types";
+
+import { ItemsContext } from "../../context/itemsContext";
 
 export function AddItemForm({ open, onClose }: AddItemFormType) {
-  const [title, setTitle] = useState("Title");
-  const [description, setDescription] = useState("Description");
-  const [value, setValue] = useState(0);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [value, setValue] = useState<null | number>(null);
+  const [type, setType] = useState<ItemType>("entry");
+  const [error, setError] = useState("");
 
   if (!open) return null;
+
+  const { createNewEntry, createNewSpent } = useContext(ItemsContext);
+
+  const handleNewItem = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (title && description && value) {
+      setError("");
+      if (type === "entry") createNewEntry({ title, description, value });
+      else createNewSpent({ title, description, value });
+    } else setError("All fields are required");
+  };
 
   return (
     <div className="fixed w-screen h-screen top-0 left-0 z-20">
       <div className="pt-16 lg:pt-32 z-30 h-screen mx-auto p-5 lg:pl-64">
         <div className="w-full md:w-[450px] bg-list-bg mx-auto mt-10 md:mt-16 rounded-xl shadow-lg animeTop">
-          <form className="p-7 flex flex-col gap-3">
+          <form onSubmit={handleNewItem} className="p-7 flex flex-col gap-3">
             <h1 className="text-center text-xl md:text-2xl">
               Add <span className="text-secondary-color">new</span> item
             </h1>
@@ -21,7 +39,7 @@ export function AddItemForm({ open, onClose }: AddItemFormType) {
               id="title"
               onChange={({ target }) => setTitle(target.value)}
               value={title}
-              placeholder={title}
+              placeholder="Item title"
               label="Item title"
               type="text"
             />
@@ -29,19 +47,23 @@ export function AddItemForm({ open, onClose }: AddItemFormType) {
               id="description"
               onChange={({ target }) => setDescription(target.value)}
               value={description}
-              placeholder={description}
+              placeholder="Your description"
               label="Item description"
               type="text"
             />
             <Input
               id="value"
               onChange={({ target }) => setValue(+target.value)}
-              value={value}
-              placeholder={String(value)}
+              value={value ? value : ""}
+              placeholder="100.00"
               label="Item value"
               type="number"
             />
-            <select className="bg-item-bg w-36 h-10 p-2 rounded-lg">
+            <select
+              className="bg-item-bg w-36 h-10 p-2 rounded-lg"
+              value={type}
+              onChange={({ target }) => setType(target.value as ItemType)}
+            >
               <option value="entry" selected>
                 Entry
               </option>
@@ -49,10 +71,11 @@ export function AddItemForm({ open, onClose }: AddItemFormType) {
                 Spent
               </option>
             </select>
+            {error && <p className="text-sm text-red-400">{error}</p>}
             <div className="flex gap-2">
               <button
                 type="submit"
-                disabled={!title || !description || value <= 0}
+                disabled={!title || !description || value === null}
                 className="bg-secondary-color px-4 py-2 w-32 text-sm mt-2 md:px-6 md:py-2 md:mt-5 md:text-base rounded-md hover:bg-secondary-color-2 disabled:opacity-60 disabled:cursor-default disabled:hover:bg-secondary-color"
               >
                 Create
--- src/shared/components/AddItemForm/AddItemForm.types.ts
+++ src/shared/components/AddItemForm/AddItemForm.types.ts
@@ -2,3 +2,5 @@ export type AddItemFormType = {
   open: boolean;
   onClose: () => void;
 };
+
+export type ItemType = "spent" | "entry";
