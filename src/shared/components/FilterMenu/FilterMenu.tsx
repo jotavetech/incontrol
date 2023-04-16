e61a1f5b0ddbffee06ec23cca3be6bdf60aaaa6c
jaoincode <ojoaogugel@gmail.com>
2023-04-16 12:32:30 -0300

    fix filter menu default value

--- src/shared/components/FilterMenu/FilterMenu.tsx
+++ src/shared/components/FilterMenu/FilterMenu.tsx
@@ -3,18 +3,20 @@ import { months } from "../../../mocks";
 export function FilterMenu() {
   return (
     <div className="mt-2 md:mt-5 flex gap-5">
-      <select className="bg-item-bg w-36 h-10 p-2 rounded-lg">
-        <option value="all" selected>
-          All
-        </option>
+      <select
+        className="bg-item-bg w-36 h-10 p-2 rounded-lg"
+        defaultValue="all"
+      >
+        <option value="all">All</option>
         {months.map((month) => (
-          <option value={month}>{month}</option>
+          <option value={month.toLowerCase()}>{month}</option>
         ))}
       </select>
-      <select className="bg-item-bg w-36 h-10 p-2 rounded-lg">
-        <option value="all" selected>
-          Sort By
-        </option>
+      <select
+        className="bg-item-bg w-36 h-10 p-2 rounded-lg"
+        defaultValue="all"
+      >
+        <option value="all">Sort By</option>
         <option value="ascending">Value (ascending)</option>
         <option value="descending">Value (descending)</option>
       </select>
