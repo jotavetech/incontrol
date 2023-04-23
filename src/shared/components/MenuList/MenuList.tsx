94967495dc7a2931f691bc99ea6da13f894028fd
jaoincode <ojoaogugel@gmail.com>
2023-04-23 11:36:03 -0300

    fix: link to

--- src/shared/components/MenuList/MenuList.tsx
+++ src/shared/components/MenuList/MenuList.tsx
@@ -10,7 +10,7 @@ export function MenuList({ type, items, loading }: MenuListType) {
   return (
     <div className="w-full md:w-[50%] h-[230px] md:h-[300px] max-h-[300px] bg-list-bg dark:bg-zinc-200 rounded-xl p-2 md:p-5 relative transition-colors">
       <Link
-        to={`/${type}`}
+        to={`/${type === "entry" ? "entries" : "spents"}`}
         className="absolute -top-10 font-semibold text-xl left-2 flex items-center gap-1"
       >
         {type === "entry" ? "Entries" : "Spents"}
